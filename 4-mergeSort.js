8665fa997e60b9a6b3b6ed087f4be5fcc8a3fbbe
jumahi <mrjasonu@gmail.com>
2023-10-01 11:39:56 +0100

    creates merge sort file

---
+++ 4-mergeSort.js
@@ -0,0 +1,13 @@
+const { dissarangedNumbers } = require('./0-random-array-gen')
+
+// Generates random array
+let arrayToSort = dissarangedNumbers(30);
+
+// Merge Sort Algorithm
+const mergeSort = arr => {
+    // function to be completed soon...
+}
+
+// // Prints Sorted Array
+console.log(`Array To Sort: \n [${arrayToSort}]`);
+console.log(`Sort Array: \n [${mergeSort(arrayToSort)}]`);
